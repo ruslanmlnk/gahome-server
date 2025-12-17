2190e73054cf83ab464b2e010293cc3a5e1e6201
Ruslan Melnyk <kanevgoga2@gmail.com>
2025-12-17 23:16:35 +0200

    set server url

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
 export default buildConfig({
+  serverURL: process.env.SERVER_URL || 'http://admin.homegudzdesign.com',
   admin: {
     user: Users.slug,
     importMap: {
